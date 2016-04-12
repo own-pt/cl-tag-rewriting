2c95a3d60ca1a424126ddd7aaf00760b5e8d0f48
Alexandre Rademaker <arademaker@gmail.com>
2016-04-12 11:23:57 -0300

    bug in the conllu converter

--- macmorpho-conllu.lisp
+++ macmorpho-conllu.lisp
@@ -4,7 +4,7 @@
 
 (defun write-conllu-lines (sent out)
   (let* ((counter 0)
-	 (lines (mapcar (lambda (tk) (list (format "~a" (incf counter))
+	 (lines (mapcar (lambda (tk) (list (write-to-string (incf counter))
 					   (car tk) "_" (cadr tk) "_" "_" "0" "_" "_" "_"))
 			sent)))
     (dolist (l lines)
@@ -15,13 +15,11 @@
 (defun convert-conllu (input output)
   (with-open-files ((out output :direction :output :if-exists :supersede)
 		    (in input))
-    (with-open-file (in filename)
-      (do ((line (read-line in nil nil)
-		 (read-line in nil nil)))
-	  ((null line)
-	   (reverse res))
-	(write-conllu-lines (mapcar (lambda (tk) (cl-ppcre:split "_" tk))
-				    (cl-ppcre:split "[ ]+" line))
-			    out)))))
+    (do ((line (read-line in nil nil)
+	       (read-line in nil nil)))
+	((null line))
+      (write-conllu-lines (mapcar (lambda (tk) (cl-ppcre:split "_" tk))
+				  (cl-ppcre:split "[ ]+" line))
+			  out))))
 
 
